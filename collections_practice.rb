4b4a629834d4c56d1ab9e725a455c008ffc9bba6
Michael Fox <michael.c.fox.ii@gmail.com>
2019-06-10 17:16:51 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,8 +2,10 @@ def sort_array_asc(array)
   array.sort
 end
 
-def sort_array_desc
-  
+def sort_array_desc(array)
+  array.sort do |left, right|
+    right <=> left
+  end
 end
 
 def sort_array_char_count
